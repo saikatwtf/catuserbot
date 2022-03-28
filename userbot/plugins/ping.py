204e0100442a945f3be4ae3015ce1c6a6fbf112b
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2022-03-28 17:03:02 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -35,7 +35,7 @@ async def _(event):
         end = datetime.now()
         ms = (end - start).microseconds / 1000
         await catevent.edit(
-            f"<b><i>🎯 𝗣𝗼𝗻𝗴</b></i>\n⚡ {ms} <b><i>ms\n👑 Bot of {hmention}</b></i>\n 🌎<b><i>DC!</b></i> <code>4</code>",
+            f"<b><i>🎯 𝗣𝗼𝗻𝗴</b></i>\n⚡ {ms} <b><i>ms\n🌎<b><i>DC!</b></i> <code>4</code>\n👑 Bot of {hmention}</b></i>",
             parse_mode="html",
         )
 

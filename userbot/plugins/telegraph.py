bf731fd1fc04b1540d9128d56561fd4601485441
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2022-03-29 07:36:18 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -84,7 +84,7 @@ async def _(event):
             await catevent.edit(
                 f"**Link: `https://telegra.ph{media_urls[0]}`\
                  \nㅤㅤㅤ\
-                 \n**Uploaded to** [Telegraph](https://telegra.ph{media_urls[0]}) **in {ms} sec by** {mention}",
+                 \n𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝚝𝚘 [𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚙𝚑](https://telegra.ph{media_urls[0]}) 𝚒𝚗 {ms} 𝚂𝚎𝚌 𝙱𝚢 {mention}",
                 link_preview=True,
             )
     elif input_str in ["text", "t"]:
